b4764b5c204630f7d1c1efeeaca53ff130a9d0da
Abhijith Prabhakar <abprabhakar@paypal.com>
2016-11-02 13:13:33 -0400

    initial commit

---
+++ src/main/java/com/example/abhiz/SwaggerRefErrorDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.abhiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerRefErrorDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerRefErrorDemoApplication.class, args);
+	}
+}
